02f80565f3e953ff3c8d8ff38de476116f64df2f
aishwarya-hub <aishwaryakumar.nexus@gmail.com>
2021-02-16 16:24:44 +0530

    removed nwidart modules

--- Modules/Channel/Resources/views/actions.blade.php
+++
@@ -1,4 +0,0 @@
-    <x-utils.view-button :href="route('Channel.show', $id)" />
-    <x-utils.edit-button :href="route('Channel.edit', $id)" />
-    <x-utils.delete-button :href="route('Channel.destroy', $id)" />
-    
--- Modules/Website/Resources/views/actions.blade.php
+++
@@ -1,4 +0,0 @@
-    <x-utils.view-button :href="route('Website.show', $id)" />
-    <x-utils.edit-button :href="route('Website.edit', $id)" />
-    <x-utils.delete-button :href="route('Website.destroy', $id)" />
-    
